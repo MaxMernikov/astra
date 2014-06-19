e9087593bcd9cd54e6be0a7ad09504ebd7000a59
max mernikov <maxmernikov@gmail.com>
2014-06-19 07:02:34 +0300

    add facebook

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,15 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
 
-    set_meta_tags title: @product.category.title, description: @product.category.detail_product, image_src: (root_url + @product.images.first.image(:preview)).gsub('//', '/')
+    set_meta_tags title:          @product.category.title,
+                  description:    @product.category.detail_product,
+                  image_src:      (root_url + @product.images.first.image(:preview)).gsub('//', '/'),
+                  og: {
+                    title:    @product.category.title,
+                    type:     'product',
+                    url:      request.original_url,
+                    image:    (root_url + @product.images.first.image(:preview)).gsub('//', '/')
+                  }
     render 'show2'
   end
 end
