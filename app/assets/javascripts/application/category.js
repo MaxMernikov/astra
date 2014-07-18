af9b1ae862828f278da3e196b7db6d0d6c493783
max mernikov <maxmernikov@gmail.com>
2014-07-19 01:24:51 +0300

    add animate

---
+++ app/assets/javascripts/application/category.js
@@ -0,0 +1,21 @@
+$( function() {
+  set_size( 500 / $('.price_list').width() );
+
+  $( window ).resize(function() {
+    set_size( 500 / $('.price_list').width() );
+  })
+
+  $('.relative').hover(
+    function(){
+      $('.relative').addClass('hover');
+      $(this).removeClass('hover');
+    },function(){
+      $('.relative').removeClass('hover');
+    })
+});
+
+function set_size(zoom_factor){
+  $('.price_list .cost').attr('style', 'font-size: ' + 73 / zoom_factor + 'px'  );
+  $('.price_list .link').attr('style', 'font-size: ' + 26 / zoom_factor + 'px'  );
+  $('.area2').attr('coords', 138/zoom_factor + ',' + 70/zoom_factor + ',' + 139/zoom_factor + ',0,' + 500/zoom_factor + ',0,' + 500/zoom_factor + ',' + 500/zoom_factor + ',0,' + 500/zoom_factor + ',0,0,' + 138/zoom_factor + ',0,' + 139/zoom_factor + ',' + 69/zoom_factor + ',' + 37/zoom_factor + ',' + 408/zoom_factor + ',' + 466/zoom_factor + ',' + 354/zoom_factor);
+}
