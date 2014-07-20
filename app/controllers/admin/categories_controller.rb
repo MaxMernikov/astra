a38a3b348cc98d97f928faba47dbae3e95574bdf
max mernikov <maxmernikov@gmail.com>
2014-07-20 21:20:51 +0300

    sd

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -46,6 +46,6 @@ private
   end
 
   def category_params
-    params.require(:category).permit(:title, :slug, :pos, :detail, :sub_detail, :detail_product, :show, :image, :plural_title)
+    params.require(:category).permit(:title, :slug, :pos, :detail, :sub_detail, :detail_product, :show, :image, :plural_title, :dative)
   end
 end
