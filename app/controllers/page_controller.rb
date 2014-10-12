3423a994cc843fabc854c84614c896f6d43fb181
max mernikov <maxmernikov@gmail.com>
2014-10-12 10:56:56 +0300

    добавил на главной ссылку на категорию

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,6 @@
 class PageController < ApplicationController
   def home
-    @five_product = Product.where(id: [18, 21, 16, 28, 27])
+    @five_product = Product.where(id: [18, 21, 16])
     # redirect_to category_path('backpacks')
   end
 end
