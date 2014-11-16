cb22ad96bdd9bda8c41ed6001ba735cf3a9b4d21
max mernikov <maxmernikov@gmail.com>
2014-11-16 08:30:04 +0300

    dg

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,11 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
 
-    set_meta_tags title:          @product.title,
+    set_meta_tags title:          "#{@product.category.dative} #{@product.title}",
                   description:    @product.description ? @product.description : @product.category.description,
                   image_src:      (root_url + @product.images.first.image(:preview)).gsub('//', '/'),
                   og: {
-                    title:    @product.title,
+                    title:    "#{@product.category.dative} #{@product.title}",
                     type:     'product',
                     url:      request.original_url,
                     image:    (root_url + @product.images.first.image(:preview)).gsub('//', '/')
