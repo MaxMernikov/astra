e2bf375276a4517625396990440ec3b2377c7d0a
max mernikov <maxmernikov@gmail.com>
2014-11-27 12:46:26 +0300

    новая главная страница

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find_by(slug: params[:id])
+    redirect_to root_path
+    # @category = Category.find_by(slug: params[:id])
 
-    set_meta_tags title: @category.title, description: @category.description, keywords: @category.keywords
+    # set_meta_tags title: @category.title, description: @category.description, keywords: @category.keywords
   end
 end
--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,7 @@
 class PageController < ApplicationController
   def home
-    @lookbook = LookbookItem.by_rows.limit(12)
+    # @lookbook = LookbookItem.by_rows.limit(12)
+    @products = Product.by_pos
+    render 'home_2'
   end
 end
