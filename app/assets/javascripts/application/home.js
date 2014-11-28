78718ec997537e34dd14d10ecfb5be07d746292b
max mernikov <maxmernikov@gmail.com>
2014-11-28 15:48:07 +0300

    добавил анимацию в шапке

--- app/assets/javascripts/application/home.js
+++ app/assets/javascripts/application/home.js
@@ -1,46 +1,49 @@
-// (function($) {
-//   $.fn.removeClassWild = function(mask) {
-//       return this.removeClass(function(index, cls) {
-//           var re = mask.replace(/\*/g, '\\S+');
-//           return (cls.match(new RegExp('\\b' + re + '', 'g')) || []).join(' ');
-//       });
-//   };
-// })(jQuery);
+var $win = $(window)
 
-// var resize_gridster = function(){
-//   $('.gridster.responsive ul').height( $('.gridster ul').width() / 3 * 2 );
-// }
-
-// $(document).ready(function() {
-//   if ($('#pagepiling').length){
-//     $('#pagepiling').pagepiling({
-//       afterLoad: function(anchorLink, index){
-//         $('body').removeClassWild('slide-*');
-//         $('body').addClass('slide-' + index);
-//       },
-//       navigation: {
-//         'position': 'right',
-//         'tooltips': []
-//       },
-//     });
-
-//     $('.slide-down').click(function(){ $('#pagepiling').pagepiling.moveSectionDown() });
-//   }
-
-//   if ($('.gridster.responsive ul').length){
-//     gridster = $(".gridster.responsive ul").gridster({
-//       widget_base_dimensions: [300, 200],
-//       widget_margins: [0, 7],
-//       helper: 'clone',
-//       max_cols: 3,
-//     }).data('gridster')
-//     resize_gridster();
-//     console.log(gridster.disable());
-
-//     $(window).resize(function(){
-//       resize_gridster();
-//     });
-//   }
+function scrolium(){
+  var full_header_h = 278
+  var small_header_h = 65
 
 
-// });
+  $scrollTop = $win.scrollTop()
+
+  if($scrollTop <= (full_header_h - small_header_h) ){
+    if ($('.header-').hasClass('small')){
+      $('.header-').removeClass('small')
+    }
+
+    $('.header-').css('height', full_header_h - $scrollTop)
+
+    var fragment_top = 0 - ($scrollTop / 1.66)
+    $('.header- .menu-fragment').css('top', (fragment_top > -121 ? fragment_top : -121) )
+
+    var small_top = 0 - ($scrollTop / 1.65)
+    $('.header- .small').css('top', (small_top > -122 ? small_top : -122) )
+
+    $('.header- .slogan').css('opacity', 1 - ($scrollTop / 100))
+    $('.header- .city').css('opacity', 0 + ($scrollTop / 100))
+
+    var full_img_opacity = 0.2 - ($scrollTop / 100 / 6)
+    $('.header- .full img').css('opacity', (full_img_opacity > 0.01 ? full_img_opacity : 0 ) )
+
+  } else {
+    if (!$('.header-').hasClass('small')){
+      $('.header-').addClass('small')
+
+      $('.header- .menu-fragment').css('top', 0)
+      $('.header-').css('height', 67)
+      $('.header- .small').css('top', 0)
+    }
+  }
+
+  if($scrollTop <= 300 ){
+    $('.header- .full').css('top', 0 - $scrollTop)
+  }
+}
+
+$(document).ready(function() {
+
+  $win.scroll(function() {
+    scrolium();
+  })
+});
