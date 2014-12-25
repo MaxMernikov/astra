d7eeded6a9bb9b772b07ad8c00d0044b6f704cb3
max mernikov <maxmernikov@gmail.com>
2014-12-25 10:39:46 +0300

    ds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,7 +13,7 @@ class ProductsController < ApplicationController
                   }
     
 
-    @random_three = Product.where(id: Product.ids.shuffle.take(3))
+    @random_three = Product.show.where(id: Product.ids.shuffle.take(3))
     return render 'show3' if @product.v3
     render 'show2'
   end
